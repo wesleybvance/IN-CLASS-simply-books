e35bebc340d86f90061b8b8a08a72c807a93d144
Wesley Vance <wslyvnce@gmail.com>
2023-01-14 12:25:01 -0600

    completed AuthorForm cpnt

---
+++ components/forms/AuthorForm.js
@@ -0,0 +1,126 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { createAuthor, updateAuthor } from '../../api/authorData';
+import { useAuth } from '../../utils/context/authContext';
+
+const initialStateAF = {
+  email: '',
+  first_name: '',
+  last_name: '',
+  image: '',
+  favorite: false,
+};
+
+export default function AuthorForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialStateAF);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateAuthor(formInput)
+        .then(() => router.push(`/author/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createAuthor(payload).then(() => {
+        router.push('/authors');
+      });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h1>{obj.firebaseKey ? 'Update' : 'Add New'} Author</h1>
+      <FloatingLabel controlId="floatingInput1" label="First Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Author's First Name"
+          name="title"
+          value={formInput.first_name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingInput1" label="Last Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Author's Last Name"
+          name="title"
+          value={formInput.last_name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Email Address"
+        className="mb-3"
+      >
+        <Form.Control
+          type="email"
+          placeholder="name@example.com"
+          name="email"
+          value={formInput.email}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingInput2" label="Author Image" className="mb-3">
+        <Form.Control
+          type="url"
+          placeholder="Enter an image url"
+          name="image"
+          value={formInput.image}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+      <Form.Check
+        className="text-white mb-3"
+        type="switch"
+        id="favoriteAuthor"
+        name="favorite"
+        label="Favorite?"
+        checked={formInput.favorite}
+        onChange={(e) => {
+          setFormInput((prevState) => ({
+            ...prevState,
+            sale: e.target.checked,
+          }));
+        }}
+      />
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create New'} Author</Button>
+    </Form>
+  );
+}
+
+AuthorForm.propTypes = {
+  obj: PropTypes.shape({
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    image: PropTypes.string,
+    favorite: PropTypes.bool,
+    firebaseKey: PropTypes.string,
+    email: PropTypes.string,
+    uid: PropTypes.string,
+  }),
+};
+
+AuthorForm.defaultProps = {
+  obj: initialStateAF,
+};
