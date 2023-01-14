c3f9587b6f71dccfab3463fcf2aca10614b0a3b7
Wesley Vance <wslyvnce@gmail.com>
2023-01-14 09:24:34 -0600

    added signout to user profile

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
 
 export default function UserProfile() {
@@ -7,10 +9,20 @@ export default function UserProfile() {
 
   return (
     <div>
-      <Image src={user.photoURL} alt="userURL" width="100px" height="100px" />
+      {/* <Image src={user.photoURL} alt="userURL" width="100px" height="100px" />
       <h1>Name: {user.displayName}</h1>
       <h3>Email: {user.email}</h3>
       <h4>Last Login: {user.metadata.lastSignInTime}</h4>
+      <Button type="button" size="lg" className="copy-btn" onClick={signOut}>Sign Out</Button> */}
+      <Card style={{ width: '40rem', margin: '10px' }}>
+        <Card.Body>
+          <Image src={user.photoURL} alt="userURL" width="100px" height="100px" />
+          <Card.Title>{user.displayName}</Card.Title>
+          <p>Email: {user.email}</p>
+          <p>Last Login: {user.metadata.lastSignInTime}</p>
+          <Button type="button" size="lg" className="copy-btn" onClick={signOut}>Sign Out</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
